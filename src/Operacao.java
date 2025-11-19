f91c5aa26c9596c3c348b75448a7e164096b7cbb
Vinícius <dlm8@ufmg.br>
2025-11-19 19:00:54 -0300

    Update Operacao.java

--- src/Operacao.java
+++ src/Operacao.java
@@ -1,26 +1,15 @@
 public class Operacao {
 
-    private char tipo;
+    private TipoOperacao tipo;
     private double valor;
 
-    public Operacao(char tipo, double valor) {
-        this.tipo = tipo;
+    public Operacao(char codigoTipo, double valor) {
+        this.tipo = TipoOperacao.buscarPorCodigo(codigoTipo);
         this.valor = valor;
     }
 
-    // TODO(#6) REFATORAR: Muita responsabilidade para mesma classe
-    public String getTipo() {
-        switch (this.tipo) {
-            case 'd':
-                return "Depósito";
-            case 's':
-                return "Saque";
-            default:
-                return null;
-        }
-    }
-
     public String toString() {
-        return this.getTipo() + ":\t" + this.valor;
+        String descricaoTipo = (this.tipo != null) ? this.tipo.getDescricao() : "Desconhecido";
+        return descricaoTipo + ":\t" + this.valor;
     }
 }
